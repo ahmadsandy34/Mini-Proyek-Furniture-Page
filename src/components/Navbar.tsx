b7e7898aa2ad4a9c743ce5ed187da6aeac4395c4
Sandy Arib Ahmad <ahmadsandyarib@gmail.com>
2024-12-13 11:15:05 +0700

    fix: fix nav image destination
    fix nav image destination

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Nav from '../assets/Nav.svg';
+import Nav from '../assets/nav.svg';
 
 const Navbar: React.FC = () => {
     const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
